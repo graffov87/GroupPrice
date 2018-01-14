7954772a32cb8df4b9c64cb8f6b8a71df7e915da
charnou <graffov87@gmail.com>
2018-01-14 20:57:11 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Group Price
+
+1. Fast changing price and qty in Grid
+2. Change Price via Cron
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Chervit_GroupPrice',
+    __DIR__
+);
